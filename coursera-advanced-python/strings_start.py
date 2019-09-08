2acf9be6b323890d502d0df9d0462084298cfd7a
Felipe Saenz <fesacor_@hotmail.com>
2019-09-08 13:56:46 -0500

    Template strings section start

--- coursera-advanced-python/strings_start.py
+++ coursera-advanced-python/strings_start.py
@@ -10,3 +10,9 @@ except Exception as e:
     print('Tried to do \'s+b\' but the error was: {}'.format(e))
     s2 = b.decode('utf-8')
     print('Decoded \'b\', This is the new concat result: {}'.format(s+s2))
+
+b2 = s.encode('utf-8')
+print('Encoding \'s\' as \'b2\' and doing \'b+b2\'. This is the result: {}'.format(b+b2))
+
+b3 = s.encode('utf-32')
+print('Encode \'s\' as utf-32. This is the result: {}'.format(b3))
